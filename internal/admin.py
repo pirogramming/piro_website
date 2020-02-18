a063620bf1a72936b851f872b75144a8f3e0f9e2
Nam Minjeong <nmj5012@naver.com>
2020-02-19 00:28:15 +0900

    Admin 전체적 수정
    - 개별 app별로 admin site 수정.

--- internal/admin.py
+++ internal/admin.py
@@ -6,19 +6,26 @@ from .models import Post, Comment, Like, InfoBook, Notification
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author']
     list_display_links = ['title']
+    search_fields = ['title']
+    list_filter = ('author',)
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['id', 'post','author']
     list_display_links = ['id']
+    search_fields = ['post__title']
+    list_filter = ('author',)
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'comment']
     list_display_links = ['id']
+    list_filter = ('user__name',)
 
 @admin.register(InfoBook)
 class InfoBookAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'piro_no']
     list_display_links = ['id']
+    search_fields = ['user__name']
+    list_filter = ('piro_no',)
 
